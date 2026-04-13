5f0d8f85e5493e80864e3e4e0ec3ec6ed8e51528
faridkasdi <farid_kasdi@yahoo.com.sg>
2026-04-13 05:47:43 +0000

    Yay, it's all finished :-D

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,39 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route("/")
-def index():
-    return render_template("main_page.html")
+# SQLite database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////home/faridkasdi/mysite/comments.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+db = SQLAlchemy(app)
+
+
+# -------------------
+# DATABASE MODEL
+# -------------------
+class Comment(db.Model):
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
+
+
+# -------------------
+# ROUTE
+# -------------------
+@app.route("/", methods=["GET", "POST"])
+def index():
+
+    if request.method == "POST":
+        comment_text = request.form["comment"]
+
+        comment = Comment(content=comment_text)
+        db.session.add(comment)
+        db.session.commit()
+
+    comments = Comment.query.all()
+
+    return render_template("main_page.html", comments=comments)
